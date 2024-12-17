66780264a6521360762739917a504fa3f79b5b18
Mohamed Hisham <mohamedhisham67751@gmail.com>
2024-12-17 11:42:41 +0200

    - Update app theme data

--- lib/core/widgets/app_text_form_field.dart
+++ lib/core/widgets/app_text_form_field.dart
@@ -18,6 +18,7 @@ class AppTextFormField extends StatelessWidget {
   final Color? cursorErrorColor;
   final Color? hoverColor;
   final Color? fillColor;
+  final Color? colorEnableBorder;
   final FormFieldValidator<String>? validator;
   final FocusNode? focusNode;
   final FocusNode? nextFocusNode;
@@ -40,6 +41,7 @@ class AppTextFormField extends StatelessWidget {
     this.fillColor,
     this.focusNode,
     this.nextFocusNode,
+    this.colorEnableBorder,
   });
 
   @override
@@ -73,10 +75,10 @@ class AppTextFormField extends StatelessWidget {
             color: Color.fromARGB(255, 139, 139, 139),
           ),
         ),
-        enabledBorder: const OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(8)),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
           borderSide: BorderSide(
-            color: Color(0xFFE8ECF4),
+            color: colorEnableBorder ?? const Color(0xFFE8ECF4),
           ),
         ),
         errorBorder: const OutlineInputBorder(
