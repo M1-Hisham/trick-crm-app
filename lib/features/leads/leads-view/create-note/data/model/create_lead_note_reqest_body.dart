c4f8b2b4c9a6755f2c8e091371acbe11c466ab0e
Mohamed Hisham <mohamedhisham67751@gmail.com>
2025-01-28 04:27:49 +0200

    - Add Create Note functionality with dependency injection and UI integration

--- lib/features/leads/leads-view/create-note/data/model/create_lead_note_reqest_body.dart
+++ lib/features/leads/leads-view/create-note/data/model/create_lead_note_reqest_body.dart
@@ -5,7 +5,7 @@ part 'create_lead_note_reqest_body.g.dart';
 @JsonSerializable()
 class CreateLeadNoteRequestBody {
   String? comment;
-  CreateLeadNoteRequestBody({this.comment});
+  CreateLeadNoteRequestBody({required this.comment});
 
   Map<String, dynamic> toJson() => _$CreateLeadNoteRequestBodyToJson(this);
 }
