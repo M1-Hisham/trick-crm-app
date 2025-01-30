d73c38ce4df44be13e1ce7031f977866cfab2e28
Mohamed Hisham <mohamedhisham67751@gmail.com>
2025-01-31 00:26:45 +0200

    - Add API service and request body and response models

---
+++ lib/features/leads/lead-view/create-lead-meeting/data/model/create_lead_meeting_model.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'create_lead_meeting_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CreateLeadMeetingModel _$CreateLeadMeetingModelFromJson(
+        Map<String, dynamic> json) =>
+    CreateLeadMeetingModel(
+      status: json['status'] as String?,
+      message: json['message'] as String?,
+    );
+
+Map<String, dynamic> _$CreateLeadMeetingModelToJson(
+        CreateLeadMeetingModel instance) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'message': instance.message,
+    };
