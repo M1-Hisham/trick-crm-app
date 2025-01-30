ed5de8fc88ea2285d6ec679e535dbeb590ae8d68
Mohamed Hisham <mohamedhisham67751@gmail.com>
2025-01-30 02:18:58 +0200

    Add functionality to API & model

--- lib/core/api/api_service.g.dart
+++ lib/core/api/api_service.g.dart
@@ -371,6 +371,42 @@ class _ApiService implements ApiService {
     return _value;
   }
 
+  @override
+  Future<DeleteAttachmentModel> deleteAttachment(
+    int id,
+    int idAttachment,
+  ) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<DeleteAttachmentModel>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/leads/${id}/${idAttachment}/delete-attachment',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late DeleteAttachmentModel _value;
+    try {
+      _value = DeleteAttachmentModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
