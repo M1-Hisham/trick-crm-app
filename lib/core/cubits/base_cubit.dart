c29d5247798b2fa05912d0b59da3368daf161ba7
Mohamed Hisham <mohamedhisham67751@gmail.com>
2025-02-10 16:03:08 +0200

    - Enhance BaseCubit and BaseRepo with API service integration for data fetching and sending

--- lib/core/cubits/base_cubit.dart
+++ lib/core/cubits/base_cubit.dart
@@ -8,11 +8,29 @@ class BaseCubit<T> extends Cubit<BaseState<T>> {
   final BaseRepo<T> _repo;
   BaseCubit(this._repo) : super(const BaseState.initial());
 
-  void getData() async {
+  /// Fetch data from API Service
+  Future<void> getData() async {
     emit(const BaseState.loading());
     log("BaseCubit: getData called");
-    final dataModel = await _repo.getData();
-    dataModel.when(
+    final response = await _repo.getData();
+    response.when(
+      success: (data) {
+        log("Data cubit: success");
+        emit(BaseState.success(data));
+      },
+      error: (e) {
+        log("Error message BaseCubit: $e");
+        emit(BaseState.error(e.toString()));
+      },
+    );
+  }
+
+  /// Send data to API Service
+  Future<void> sendData(dynamic requestBody) async {
+    emit(const BaseState.loading());
+    log("BaseCubit: sendData called");
+    final response = await _repo.sendData(requestBody);
+    response.when(
       success: (data) {
         log("Data cubit: success");
         emit(BaseState.success(data));
--- lib/core/repo/base_repo.dart
+++ lib/core/repo/base_repo.dart
@@ -5,13 +5,38 @@ import 'package:trick_crm_app/core/api/api_result.dart';
 /// Generic repo class
 class BaseRepo<T> {
   /// Fetch data from API Service
-  final Future<T> Function() _fetchData;
+  final Future<T> Function()? _fetchData;
 
-  BaseRepo(this._fetchData);
+  /// Send data to API Service
+  final Future<T> Function(dynamic requestBody)? _sendData;
 
+  BaseRepo(
+      {Future<T> Function()? fetchData, Future<T> Function(dynamic)? sendData})
+      : _fetchData = fetchData,
+        _sendData = sendData;
+
+  /// @GET
   Future<ApiResult<T>> getData() async {
     try {
+      if (_fetchData == null) {
+        throw Exception("No fetchData function provided!");
+      }
       final response = await _fetchData();
+      log("Base Repo: success");
+      return ApiResult.success(response);
+    } catch (e) {
+      log("Error in Base repo: $e");
+      return ApiResult.error(e.toString());
+    }
+  }
+
+  /// @POST
+  Future<ApiResult<T>> sendData(dynamic requestBody) async {
+    try {
+      if (_sendData == null) {
+        throw Exception("No sendData function provided!");
+      }
+      final response = await _sendData(requestBody);
       log("Base Repo: success");
       return ApiResult.success(response);
     } catch (e) {
